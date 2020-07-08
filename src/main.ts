9490957067b90c4cdb35d235aa665bcfb7c2813d
aki <lala.akira@gmail.com>
2020-07-08 09:13:26 +0900

    First Release

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,70 @@
+import * as cache from '@actions/cache'
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as os from 'os'
+import * as fs from 'fs'
+import * as stream from 'stream'
+import * as crypto from 'crypto'
+import * as util from 'util'
+import execa from 'execa'
+
+const getNodeVersion = async (): Promise<string> => {
+  const {stdout} = await execa.command('node --version')
+  return stdout
+}
+
+const yarnCacheDir = async (): Promise<string> => {
+  const {stdout} = await execa.command('yarn cache dir')
+  return stdout
+}
+
+const hashFile = async (file: string): Promise<string> => {
+  const hash = crypto.createHash('sha256')
+  const pipeline = util.promisify(stream.pipeline)
+  await pipeline(fs.createReadStream(file), hash)
+  return hash.digest('hex')
+}
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    const cacheKeyPrefix = core.getInput('cacheKeyPrefix', {required: true})
+    const baseKey = [
+      os.platform,
+      'node',
+      await getNodeVersion(),
+      cacheKeyPrefix
+    ].join('-')
+    const key = `${baseKey}-${await hashFile('yarn.lock')}`
+    const restoreKeys = [baseKey]
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const paths = [await yarnCacheDir(), 'node_modules']
 
-    core.setOutput('time', new Date().toTimeString())
+    const cacheKey = await cache.restoreCache(paths, key, restoreKeys)
+    const cacheHit = !!cacheKey
+    core.setOutput('cache-hit', cacheHit.toString())
+
+    if (cacheKey) {
+      core.debug(`cache hit: ${cacheKey}`)
+      return
+    }
+
+    const installCommand = core.getInput('installCommand', {
+      required: true
+    })
+    await execa.command(installCommand)
+
+    // Error handling from https://github.com/actions/cache/blob/master/src/save.ts
+    core.info('Saving cache')
+    try {
+      await cache.saveCache(paths, key)
+    } catch (error) {
+      if (error.name === cache.ValidationError.name) {
+        throw error
+      } else if (error.name === cache.ReserveCacheError.name) {
+        core.info(error.message)
+      } else {
+        core.info(`[warning]${error.message}`)
+      }
+    }
   } catch (error) {
     core.setFailed(error.message)
   }
